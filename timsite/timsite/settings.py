597af70e5c66ca5fc8d44230ce6c71c6c9a3519b
Ubuntu <ubuntu@ip-172-26-6-141.us-east-2.compute.internal>
2018-02-19 04:47:51 +0000

    changed settings

--- timsite/timsite/settings.py
+++ timsite/timsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1a)9tqg$(xcdt*%2bjt##*zeuxbek5dqwt(zkuvx4ab2(g4!lm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.theindependentman.com','127.0.0.1']
+ALLOWED_HOSTS = ['www.theindependentman.com','127.0.0.1','13.58.197.21']
 
 
 # Application definition
@@ -61,7 +61,7 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+               'django.contrib.messages.context_processors.messages',
             ],
         },
     },
